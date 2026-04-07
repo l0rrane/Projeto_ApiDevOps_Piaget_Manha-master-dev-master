19e1ca530bcd40952d716dbd8e104e99367eca07
l0rrane <gabrielalorraneespin@gmail.com>
2026-04-07 10:19:22 -0300

    atualizado

---
+++ AulaApiDevOpsManhaPiaget/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace AulaApiDevOpsManhaPiaget.Models
+{
+    public class Categoria : Base
+    {
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
